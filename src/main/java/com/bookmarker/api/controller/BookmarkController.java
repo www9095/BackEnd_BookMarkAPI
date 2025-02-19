e4f09fadbc0af9489e6ccafba8c9f5191ad9254f
www9095 <www9095@gmail.com>
2025-02-19 14:45:26 +0900

    page param 적용

--- src/main/java/com/bookmarker/api/controller/BookmarkController.java
+++ src/main/java/com/bookmarker/api/controller/BookmarkController.java
@@ -5,6 +5,7 @@ import com.bookmarker.api.service.BookmarkService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,8 +16,9 @@ import java.util.List;
 public class BookmarkController {
     private final BookmarkService bookmarkService;
 
+    //api/bookmarks?page=2
     @GetMapping
-    public List<Bookmark> getBookmarks() {
-        return bookmarkService.getBookmarks();
+    public List<Bookmark> getBookmarks(@RequestParam(name = "page", defaultValue = "1") Integer page) {
+        return bookmarkService.getBookmarks(page);
     }
 }
--- src/main/java/com/bookmarker/api/service/BookmarkService.java
+++ src/main/java/com/bookmarker/api/service/BookmarkService.java
@@ -3,6 +3,9 @@ package com.bookmarker.api.service;
 import com.bookmarker.api.domain.Bookmark;
 import com.bookmarker.api.domain.BookmarkRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,8 +18,12 @@ public class BookmarkService {
     private final BookmarkRepository repository;
 
     @Transactional(readOnly = true)
-    public List<Bookmark> getBookmarks() {
-        return repository.findAll();
+    public List<Bookmark> getBookmarks(Integer page) {
+        //JPA의 페이지번호가 0부터 시작하기 때문에(클라이언트는 1부터)
+        int pageNo = page < 1 ? 0 : page - 1;
+        Pageable pageable = PageRequest.of(pageNo, 10, Sort.Direction.DESC, "id");
+        return repository.findAll(pageable) //Page<T>
+                         .getContent(); //page내부 데이터 추출
     }
     
 }
