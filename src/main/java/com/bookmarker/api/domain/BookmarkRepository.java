e462c61625f2b494bed877d33a552db91b2cfa91
www9095 <www9095@gmail.com>
2025-02-20 09:40:58 +0900

    interface projection

--- src/main/java/com/bookmarker/api/domain/BookmarkRepository.java
+++ src/main/java/com/bookmarker/api/domain/BookmarkRepository.java
@@ -1,6 +1,7 @@
 package com.bookmarker.api.domain;
 
 import com.bookmarker.api.dto.BookmarkDTO;
+import com.bookmarker.api.dto.BookmarkVM;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -24,6 +25,9 @@ public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
     Page<BookmarkDTO> searchBookmarks(String query, Pageable pageable);
 
     //method명의 Naming 규칙에 따라서 Query문을 자동으로 생성해주는 Query Method
-    //Contains : % %, IgnoreCase : 대소문자를 구분하지 않기
+    // findBy : prefix, Ttle : 필드 변수명
+    //Contains : like % %, IgnoreCase : 대소문자를 구분하지 않기
     Page<BookmarkDTO> findByTitleContainsIgnoreCase(String query, Pageable pageable);
+
+    Page<BookmarkVM> findByTitleContainingIgnoreCase(String query, Pageable pageable);
 }
--- src/main/java/com/bookmarker/api/service/BookmarkService.java
+++ src/main/java/com/bookmarker/api/service/BookmarkService.java
@@ -4,6 +4,7 @@ import com.bookmarker.api.domain.Bookmark;
 import com.bookmarker.api.domain.BookmarkRepository;
 import com.bookmarker.api.dto.BookmarkDTO;
 import com.bookmarker.api.dto.BookmarkMapper;
+import com.bookmarker.api.dto.BookmarkVM;
 import com.bookmarker.api.dto.BookmarksDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -42,12 +43,13 @@ public class BookmarkService {
     }
 
     @Transactional(readOnly = true)
-    public BookmarksDTO searchBookmarks(String query, Integer page) {
+    public BookmarksDTO<?> searchBookmarks(String query, Integer page) {
         int pageNo = page < 1 ? 0 : page - 1 ;
         Pageable pageable = PageRequest.of(pageNo, 10, Sort.Direction.DESC, "createdAt");
 //        Page<BookmarkDTO> bookmarkPage = repository.searchBookmarks(query, pageable);
-        Page<BookmarkDTO> bookmarkPage = repository.findByTitleContainsIgnoreCase(query, pageable);
-        return new BookmarksDTO(bookmarkPage);
+//        Page<BookmarkDTO> bookmarkPage = repository.findByTitleContainsIgnoreCase(query, pageable);
+        Page<BookmarkVM> bookmarkPage = repository.findByTitleContainingIgnoreCase(query, pageable);
+        return new BookmarksDTO<>(bookmarkPage);
     }
     
 }
